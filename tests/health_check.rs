1ef10fd789748e102a98504c395e23a05e9dca61
aashish2057 <aashish2057@gmail.com>
2023-05-17 18:41:56 -0700

    fix subscription

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -87,7 +87,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     assert_eq!(200, response.status().as_u16());
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+    let saved = sqlx::query!("SELECT email, name FROM subscription",)
         .fetch_one(&mut connection)
         .await
         .expect("Failed to fetch subscriptions");
